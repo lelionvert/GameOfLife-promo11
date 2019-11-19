63484e86579dad623be63ced6c56ceaebbf1e4b9
talialac <nicolas.dfr@gmail.com>
2019-11-19 16:29:04 +0100

    Delete unused

--- src/main/java/fr/lacombe/Neighbors.java
+++ src/main/java/fr/lacombe/Neighbors.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 public class Neighbors {
     private ArrayList<Cell> cells;
-    private int MAX_NEIGHBORS = 8;
 
     Neighbors() {
         this.cells = new ArrayList<>();
@@ -31,7 +30,6 @@ public class Neighbors {
     public String toString() {
         final StringBuilder sb = new StringBuilder("Neighbors{");
         sb.append("cells=").append(cells);
-        sb.append(", MAX_NEIGHBORS=").append(MAX_NEIGHBORS);
         sb.append('}');
         return sb.toString();
     }
