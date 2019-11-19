f1ab508e9ba96a1657463b5d2285e85b110609a4
talialac <nicolas.dfr@gmail.com>
2019-11-19 17:41:47 +0100

    New test create_Grid_with_two_cells_one_dead_one_living_should_return_grid_with_dead_cells

---
+++ src/test/java/fr/lacombe/GridTest.java
@@ -0,0 +1,9 @@
+package fr.lacombe;
+
+import org.junit.jupiter.api.Test;
+
+public class GridTest {
+    @Test
+    void create_Grid_with_two_cells_one_dead_one_living_should_return_grid_with_dead_cells() {
+    }
+}
