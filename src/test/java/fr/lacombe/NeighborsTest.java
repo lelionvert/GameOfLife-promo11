5e23e9e3a887401b6b7ee4f6cff6748e6ddf822b
talialac <nicolas.dfr@gmail.com>
2019-11-19 17:11:05 +0100

    Add test living Neighbors

---
+++ src/test/java/fr/lacombe/NeighborsTest.java
@@ -0,0 +1,12 @@
+package fr.lacombe;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class NeighborsTest {
+
+    @Test
+    public void living_should_return_zero_if_cell_has_dead_neighbors() {
+
+    }
+}
